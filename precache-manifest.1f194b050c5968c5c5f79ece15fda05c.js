81c7e36b8ec668d63ec2fdbcb7ce2fab5e045ec2
Sami Almaradni <samialmaradni@Samis-MBP.fritz.box>
2019-03-01 10:06:22 +0100

    Updates

---
+++ precache-manifest.1f194b050c5968c5c5f79ece15fda05c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/to-do-list/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9d55d607ae1fdfc75248",
+    "url": "/to-do-list/static/js/runtime~main.9d55d607.js"
+  },
+  {
+    "revision": "56cd370dc4c6806729e0",
+    "url": "/to-do-list/static/js/main.56cd370d.chunk.js"
+  },
+  {
+    "revision": "1c3bccd22e4eec3e27da",
+    "url": "/to-do-list/static/js/2.1c3bccd2.chunk.js"
+  },
+  {
+    "revision": "56cd370dc4c6806729e0",
+    "url": "/to-do-list/static/css/main.40ace7e3.chunk.css"
+  },
+  {
+    "revision": "f680c4a366c88dcdbfac9ec2e7f9455a",
+    "url": "/to-do-list/index.html"
+  }
+];
